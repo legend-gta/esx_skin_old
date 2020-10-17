b2ba29981f345727e10957208616a57ab4335862
legend-gta <iamcreativeuser@gmail.com>
2020-10-17 17:36:02 +0530

    updated

--- client/main.lua
+++ client/main.lua
@@ -1,12 +1,6 @@
-ESX                  = nil
-local FirstSpawn     = true
-local LastSkin       = nil
-local PlayerLoaded   = false
-local cam 			     = nil
-local isCameraActive = false
-local zoomOffset	   = 0.0
-local camOffset 	   = 0.0
-local heading 		   = 90.0
+ESX = nil
+local lastSkin, playerLoaded, cam, isCameraActive
+local firstSpawn, zoomOffset, camOffset, heading = true, 0.0, 0.0, 90.0
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -16,15 +10,13 @@ Citizen.CreateThread(function()
 end)
 
 function OpenMenu(submitCb, cancelCb, restrict)
-
-	local playerPed = GetPlayerPed(-1)
+	local playerPed = PlayerPedId()
 
 	TriggerEvent('skinchanger:getSkin', function(skin)
-		LastSkin = skin
+		lastSkin = skin
 	end)
 
 	TriggerEvent('skinchanger:getData', function(components, maxVals)
-
 		local elements    = {}
 		local _components = {}
 
@@ -35,7 +27,6 @@ function OpenMenu(submitCb, cancelCb, restrict)
 			end
 		else
 			for i=1, #components, 1 do
-
 				local found = false
 
 				for j=1, #restrict, 1 do
@@ -47,18 +38,16 @@ function OpenMenu(submitCb, cancelCb, restrict)
 				if found then
 					table.insert(_components, components[i])
 				end
-
 			end
 		end
 
 		-- Insert elements
 		for i=1, #_components, 1 do
-
 			local value       = _components[i].value
 			local componentId = _components[i].componentId
 
 			if componentId == 0 then
-				value = GetPedPropIndex(playerPed,  _components[i].componentId)
+				value = GetPedPropIndex(playerPed, _components[i].componentId)
 			end
 
 			local data = {
@@ -75,99 +64,84 @@ function OpenMenu(submitCb, cancelCb, restrict)
 			for k,v in pairs(maxVals) do
 				if k == _components[i].name then
 					data.max = v
+					break
 				end
 			end
 
 			table.insert(elements, data)
-
 		end
 
 		CreateSkinCam()
 		zoomOffset = _components[1].zoomOffset
 		camOffset = _components[1].camOffset
 
-		ESX.UI.Menu.Open(
-			'default', GetCurrentResourceName(), 'skin',
-			{
-				title = _U('skin_menu'),
-				align = 'top-left',
-				elements = elements
-			},
-			function(data, menu)
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'skin', {
+			title    = _U('skin_menu'),
+			align    = 'top-left',
+			elements = elements
+		}, function(data, menu)
+			TriggerEvent('skinchanger:getSkin', function(skin)
+				lastSkin = skin
+			end)
 
-				TriggerEvent('skinchanger:getSkin', function(skin)
-					LastSkin = skin
+			submitCb(data, menu)
+			DeleteSkinCam()
+		end, function(data, menu)
+			menu.close()
+			DeleteSkinCam()
+			TriggerEvent('skinchanger:loadSkin', lastSkin)
+
+			if cancelCb ~= nil then
+				cancelCb(data, menu)
+			end
+		end, function(data, menu)
+			local skin, components, maxVals
+
+			TriggerEvent('skinchanger:getSkin', function(getSkin)
+				skin = getSkin
+			end)
+
+			zoomOffset = data.current.zoomOffset
+			camOffset = data.current.camOffset
+
+			if skin[data.current.name] ~= data.current.value then
+				-- Change skin element
+				TriggerEvent('skinchanger:change', data.current.name, data.current.value)
+
+				-- Update max values
+				TriggerEvent('skinchanger:getData', function(comp, max)
+					components, maxVals = comp, max
 				end)
 
-				submitCb(data, menu)
-				DeleteSkinCam()
-			end,
-			function(data, menu)
+				local newData = {}
 
-				menu.close()
+				for i=1, #elements, 1 do
+					newData = {}
+					newData.max = maxVals[elements[i].name]
 
-				DeleteSkinCam()
-
-				TriggerEvent('skinchanger:loadSkin', LastSkin)
-
-				if cancelCb ~= nil then
-					cancelCb(data, menu)
-				end
-
-			end,
-			function(data, menu)
-
-				TriggerEvent('skinchanger:getSkin', function(skin)
-
-					zoomOffset = data.current.zoomOffset
-					camOffset = data.current.camOffset
-
-					if skin[data.current.name] ~= data.current.value then
-
-						-- Change skin element
-						TriggerEvent('skinchanger:change', data.current.name, data.current.value)
-
-						-- Update max values
-						TriggerEvent('skinchanger:getData', function(components, maxVals)
-
-							for i=1, #elements, 1 do
-
-								local newData = {}
-								
-								newData.max = maxVals[elements[i].name]
-
-								if elements[i].textureof ~= nil and data.current.name == elements[i].textureof then
-									newData.value = 0
-								end
-
-								menu.update({name = elements[i].name}, newData)
-
-							end
-
-							menu.refresh()
-
-						end)
-
+					if elements[i].textureof ~= nil and data.current.name == elements[i].textureof then
+						newData.value = 0
 					end
 
-				end)
+					menu.update({name = elements[i].name}, newData)
+				end
 
-			end,
-			function()
-				DeleteSkinCam()
+				menu.refresh()
 			end
-		)
-
+		end, function(data, menu)
+			DeleteSkinCam()
+		end)
 	end)
-
 end
 
 function CreateSkinCam()
 	if not DoesCamExist(cam) then
 		cam = CreateCam('DEFAULT_SCRIPTED_CAMERA', true)
 	end
+
 	SetCamActive(cam, true)
 	RenderScriptCams(true, true, 500, true, true)
+
 	isCameraActive = true
 	SetCamRot(cam, 0.0, 0.0, 270.0, true)
 	SetEntityHeading(playerPed, 90.0)
@@ -183,6 +157,7 @@ end
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
+
 		if isCameraActive then
 			DisableControlAction(2, 30, true)
 			DisableControlAction(2, 31, true)
@@ -190,11 +165,10 @@ Citizen.CreateThread(function()
 			DisableControlAction(2, 33, true)
 			DisableControlAction(2, 34, true)
 			DisableControlAction(2, 35, true)
+			DisableControlAction(0, 25, true) -- Input Aim
+			DisableControlAction(0, 24, true) -- Input Attack
 
-			DisableControlAction(0, 25,   true) -- Input Aim
-    		DisableControlAction(0, 24,   true) -- Input Attack
-
-			local playerPed = GetPlayerPed(-1)
+			local playerPed = PlayerPedId()
 			local coords    = GetEntityCoords(playerPed)
 
 			local angle = heading * math.pi / 180.0
@@ -202,9 +176,10 @@ Citizen.CreateThread(function()
 				x = math.cos(angle),
 				y = math.sin(angle)
 			}
+
 			local pos = {
 				x = coords.x + (zoomOffset * theta.x),
-				y = coords.y + (zoomOffset * theta.y),
+				y = coords.y + (zoomOffset * theta.y)
 			}
 
 			local angleToLook = heading - 140.0
@@ -213,111 +188,105 @@ Citizen.CreateThread(function()
 			elseif angleToLook < 0 then
 				angleToLook = angleToLook + 360
 			end
+
 			angleToLook = angleToLook * math.pi / 180.0
 			local thetaToLook = {
 				x = math.cos(angleToLook),
 				y = math.sin(angleToLook)
 			}
+
 			local posToLook = {
 				x = coords.x + (zoomOffset * thetaToLook.x),
-				y = coords.y + (zoomOffset * thetaToLook.y),
+				y = coords.y + (zoomOffset * thetaToLook.y)
 			}
 
 			SetCamCoord(cam, pos.x, pos.y, coords.z + camOffset)
 			PointCamAtCoord(cam, posToLook.x, posToLook.y, coords.z + camOffset)
 
-			SetTextComponentFormat("STRING")
-			AddTextComponentString(_U('use_rotate_view'))
-			DisplayHelpTextFromStringLabel(0, 0, 0, -1)
+			ESX.ShowHelpNotification(_U('use_rotate_view'))
+		else
+			Citizen.Wait(500)
 		end
 	end
 end)
 
 Citizen.CreateThread(function()
 	local angle = 90
+
 	while true do
 		Citizen.Wait(0)
+
 		if isCameraActive then
 			if IsControlPressed(0, 108) then
 				angle = angle - 1
 			elseif IsControlPressed(0, 109) then
 				angle = angle + 1
 			end
+
 			if angle > 360 then
 				angle = angle - 360
 			elseif angle < 0 then
 				angle = angle + 360
 			end
+
 			heading = angle + 0.0
+		else
+			Citizen.Wait(500)
 		end
 	end
 end)
 
 function OpenSaveableMenu(submitCb, cancelCb, restrict)
-
 	TriggerEvent('skinchanger:getSkin', function(skin)
-		LastSkin = skin
+		lastSkin = skin
 	end)
 
 	OpenMenu(function(data, menu)
-
 		menu.close()
-
 		DeleteSkinCam()
 
 		TriggerEvent('skinchanger:getSkin', function(skin)
-
 			TriggerServerEvent('esx_skin:save', skin)
 
 			if submitCb ~= nil then
 				submitCb(data, menu)
 			end
-
 		end)
 
 	end, cancelCb, restrict)
-
 end
 
 AddEventHandler('playerSpawned', function()
-
 	Citizen.CreateThread(function()
-
-		while not PlayerLoaded do
-			Citizen.Wait(0)
+		while not playerLoaded do
+			Citizen.Wait(100)
 		end
 
-		if FirstSpawn then
-
+		if firstSpawn then
 			ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
-
 				if skin == nil then
-					TriggerEvent('skinchanger:loadSkin', {sex = 0}, OpenSaveableMenu)
+			      ESX.ShowHelpNotification('Welcome')
 				else
 					TriggerEvent('skinchanger:loadSkin', skin)
 				end
-
 			end)
 
-			FirstSpawn = false
-
+			firstSpawn = false
 		end
-
 	end)
-
 end)
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
-	PlayerLoaded = true
+	playerLoaded = true
 end)
 
 AddEventHandler('esx_skin:getLastSkin', function(cb)
-	cb(LastSkin)
+	cb(lastSkin)
 end)
 
 AddEventHandler('esx_skin:setLastSkin', function(skin)
-	LastSkin = skin
+	lastSkin = skin
 end)
 
 RegisterNetEvent('esx_skin:openMenu')
@@ -345,18 +314,4 @@ AddEventHandler('esx_skin:requestSaveSkin', function()
 	TriggerEvent('skinchanger:getSkin', function(skin)
 		TriggerServerEvent('esx_skin:responseSaveSkin', skin)
 	end)
-end)
-
-Citizen.CreateThread(function()
-	while true do
-
-		Citizen.Wait(0)
-
-		local playerPed = GetPlayerPed(-1)
-
-		if IsEntityDead(playerPed) then
-			HasLoadedModel = false
-		end
-
-	end
 end)
--- locales/en.lua
+++ locales/en.lua
@@ -1,8 +1,6 @@
 Locales['en'] = {
-
-	['skin_menu'] = 'Skin Menu',
-	['use_rotate_view'] = 'use ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ and ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ to rotate the view.',
-	['skin'] = 'change skin',
-	['saveskin'] = 'save skin to a file',
-
+  ['skin_menu'] = 'Apariencia',
+  ['use_rotate_view'] = 'Usa ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar la vista.',
+  ['skin'] = 'Cambiar apariencia',
+  ['saveskin'] = 'Guardar apariencia',
 }
